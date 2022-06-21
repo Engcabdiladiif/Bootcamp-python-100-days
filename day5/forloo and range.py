ebdf28484fafd4e85a0ac9d80ffecf65010c8197
Engcabdiladiif <cabdiladiifsaalax988@gmail.com>
2022-06-21 07:39:51 -0700

    for loop and range

---
+++ day5/forloo and range.py
@@ -0,0 +1,87 @@
+# for loop and range
+# building passoword generator
+
+
+# 2.  friuts = ["apple", "peach","pear"]
+# for frut in friuts:
+#     print(frut)
+#     print(frut + "pie")
+#     print(friuts)
+
+
+#  3.  Average Height Exercise
+ # calculating the average student height
+
+# student_heights = input("input a list of student height ").split()
+#
+# for n in range(0 ,len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+#
+# total_height = 0
+# for height in student_heights:
+#     total_height += height
+# print(total_height)
+
+# ma isticmaalaysid sum and len
+
+# 6. the intertivate code Exercise  High sore
+
+# student_scores = input("input the a list of student score ").split()
+# for n in range(0, len(student_scores)):
+#      student_scores[n] = int(student_scores[n])
+# print(student_scores)
+#
+# highest_score = 0
+# for score in student_scores:
+#     if score > highest_score:
+#         highest_score = score
+# print(f"The highest score in the class is : {highest_score}")
+
+# 5.   for loop and range
+
+# for number in range(0, 101):
+#     print(number)
+
+# saddex tiro bood
+
+# for number in range(1, 11, 3):
+#     print(number)
+
+# total = 0
+# for number in range(1, 101):
+#     total += number
+#     print(total)
+
+# 6 .  Add Event
+# total = 0
+# for number in range(2,101 ,2):
+#     print(number)
+
+# another way
+
+# total = 0
+# for number in range(2, 101, 2):
+#     total += number
+# print(total)
+#
+# # another way
+#
+# total2 = 0
+#
+# for number in range(1, 101):
+#     if number % 2 == 0:
+#         total2 += number
+# print(total2)
+
+# 7.  cod execrise  the fizzBuzz job interview Question
+
+# for number in range(1,101):
+#     if number % 3 == 0 and number %5 == 0:
+#         print("fizzBuzz")
+#     elif number %3 == 0:
+#         print("Fizz")
+#     elif number %5 ==0:
+#         print("Buzz")
+#     else:
+#         print(number)
+
