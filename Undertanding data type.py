751d3524fc24edb51b45a9ecbd8ea26b1d07af31
Engcabdiladiif <cabdiladiifsaalax988@gmail.com>
2022-09-25 08:37:07 -0700

    intro/conditinal

---
+++ Undertanding data type.py
@@ -0,0 +1,117 @@
+# Understanding the data type
+# Data type numbers,Operations Type Conversion f-string
+
+
+##002 len
+
+# print(len("hellow world"))
+
+# string
+# print("welcome")
+
+#accesing the item
+# print("welcome"[4])
+
+#concentintes
+# print("123" + "345")
+#
+# print(123 + 456)
+
+# float
+# print(3.1456)
+
+#flaoting point Number
+
+# print(3141.59)
+
+# Boolean
+# True false
+
+# Type of error and type checking and type convert
+#
+# nmu_char = len(input("what is your name: "))
+# # print("your name has "+ nmu_char + "charcter")
+# new_char = str(nmu_char)
+# print(new_char)
+
+# 005  code challeneg Data type
+#
+# two_digit_number = input("enter two digit Number: ")
+#
+# first_number = two_digit_number[0]
+# secion_number = two_digit_number[1]
+#
+# total = int(first_number) + int(secion_number)
+# print(total)
+
+# 006   Mathecmatics operation
+
+# 3 + 5
+# 7 - 4
+# 3 * 2
+#
+# 2 ** 3 # Exponent
+# print(6/4)
+
+# PEMDAS
+
+# Example
+# print(3 * (3 + 3)  / 3 - 3)
+# 007
+#
+# height = input("Enter your height: ")
+# weight = input("Enter your weight: ")
+#
+# Bmi = int(weight) / float(height) ** 2
+# print(Bmi)
+
+# 008 Number  mainpulation and f string in pyhon
+
+# round number
+
+# print(round(8/3))
+# print(round (5/ 3,45))
+
+# floor
+# print(8 // 3
+
+# resul = 4/ 2
+# resul /= 2
+# print(resul)
+
+# f string
+
+# resul = 4/ 2
+# resul /= 2
+# print(f"your resul is {resul}")
+
+# code challenge life week
+
+# age  = input("what is your age: ")
+# age_as_int = int(age)
+#
+# years_remaining = 90 - age_as_int
+# days_remainig = years_remaining * 365
+# week_remaining = years_remaining * 15
+# months_remaining = years_remaining * 12
+# print(f"you have{days_remainig} days , {week_remaining} weeks,{months_remaining} months")
+
+# Dhisida calculator
+#
+# first_name = int(input("Enter the first Number: "))
+# operator = input("Enter the operator: ")
+# section_number = int(input("Enter the section number: "))
+#
+# if operator =="+":
+#     print(first_name + section_number)
+# elif operator =="-":
+#     print(first_name - section_number)
+# elif operator =="*":
+#     print(first_name * section_number)
+# elif operator =="/":
+#     print(first_name  / section_number)
+# elif operator == "**":
+#     print(first_name ** section_number)
+# else:
+#     print("this ereror")
+
